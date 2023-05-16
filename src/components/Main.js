bc260e887ef73d6dccb526accd27bcacbd9302b9
arjybltrn <arjaybeltran16@gmail.com>
2023-05-16 10:57:18 -0400

    frontend first commit

---
+++ src/components/Main.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+
+const Main = (props) => {
+    
+    const [ people, setPeople ] = useState(null)
+    // const url = "http://localhost:4000/people/"
+    const url = "https://person-app-backend-n1kg.onrender.com/people"
+
+    const getPeople = async () => {
+        const response = await fetch(url)
+        const data = await response.json()
+        setPeople(data)
+    }
+    
+    
+    const createPeople = async (person) => {
+        await fetch(url, { 
+            method: "POST",
+            headers: { 
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(person),
+            
+        })
+        getPeople()
+    }
+
+
+    const updatePeople = async (person, id) => {
+        await fetch (url + id, {
+            method: "PUT", 
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(person),
+        })
+        getPeople()
+    }
+
+    const deletePeople = async id => {
+        await fetch(url + id, {
+            method: "DELETE",
+        })
+        getPeople()
+    }
+
+    useEffect(() => getPeople, [])
+    
+    
+    return (
+    <main>
+    <Routes> 
+        <Route exact path="/" element={<Index people={people} createPeople={createPeople} />} />
+        <Route path="/people/:id" element={<Show people={people} updatePeople={updatePeople} deletePeople={deletePeople}/> } /> 
+    </Routes>
+    </main>
+
+
+      );
+}
+
+export default Main
---
+++ src/pages/Show.js
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const Show = (props) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const people = props.people;
+  const person = people.find((p) => p._id === id)
+
+  const [ editForm, setEditForm ] = useState(person)
+
+  const [ isEditing, setisEditing ] = useState(false) 
+  
+  useEffect(() => {
+    if (person) {
+      setEditForm(person)
+    } 
+  }, [person])
+
+  const handleChange = (event) => {
+    setEditForm({ ...editForm, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.updatePeople(editForm, person._id);
+    navigate("/");
+  }
+
+  const removePerson = () => {
+    props.deletePeople(person._id);
+    navigate("/");
+  }
+
+  const handleEdit = () => {
+    setisEditing(prevState => !prevState)
+  }
+
+  const loaded = () => {
+    return (
+      <>
+        <h1>{person.name}</h1>
+        <h2>{person.title}</h2>
+        <img 
+          className="avatar-image" 
+          src={person.image} 
+          alt={person.name} 
+        />
+        <button onClick={handleEdit}> { isEditing ? 'Cancel Edit' : 'Edit'}</button>
+        <button onClick={removePerson}>Delete</button>
+      </>
+    );
+  };
+  const loading = () => {
+    return <h1>Loading ...</h1>;
+  };
+
+  return (
+    <div className="person">
+      { person ? loaded() : loading() }
+
+      { isEditing && 
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={editForm.name}
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.image}
+          name="image"
+          placeholder="image URL"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.title}
+          name="title"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Update Person" />
+      </form>
+    }
+
+    </div>
+  );
+};
+
+export default Show;
