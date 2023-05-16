0910c9d69bc3a9ce6f0f63614964a8cd77be8402
arjybltrn <arjaybeltran16@gmail.com>
2023-05-16 12:17:38 -0400

    show reverts to original

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -2,23 +2,20 @@ import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const Show = (props) => {
-  console.log(id)
   const { id } = useParams();
   const navigate = useNavigate();
   const people = props.people;
-  console.log(people)
-  const person = people.find((p) => p._id === id);
+  const person = people.find((p) => p._id === id)
 
-  const [editForm, setEditForm] = useState(person);
-  const [isEditing, setisEditing] = useState(false);
-  const [isLoading, setIsLoading] = useState(true); // Add isLoading state
+  const [ editForm, setEditForm ] = useState(person)
 
+  const [ isEditing, setisEditing ] = useState(false) 
+  
   useEffect(() => {
     if (person) {
-      setEditForm(person);
-      setIsLoading(false); // Set isLoading to false when person is available
-    }
-  }, [person]);
+      setEditForm(person)
+    } 
+  }, [person])
 
   const handleChange = (event) => {
     setEditForm({ ...editForm, [event.target.name]: event.target.value });
@@ -28,63 +25,67 @@ const Show = (props) => {
     event.preventDefault();
     props.updatePeople(editForm, person._id);
     navigate("/");
-  };
+  }
 
   const removePerson = () => {
     props.deletePeople(person._id);
     navigate("/");
-  };
+  }
 
   const handleEdit = () => {
-    setisEditing((prevState) => !prevState);
-  };
+    setisEditing(prevState => !prevState)
+  }
 
   const loaded = () => {
     return (
       <>
         <h1>{person.name}</h1>
         <h2>{person.title}</h2>
-        <img className="avatar-image" src={person.image} alt={person.name} />
-        <button onClick={handleEdit}>{isEditing ? "Cancel Edit" : "Edit"}</button>
+        <img 
+          className="avatar-image" 
+          src={person.image} 
+          alt={person.name} 
+        />
+        <button onClick={handleEdit}> { isEditing ? 'Cancel Edit' : 'Edit'}</button>
         <button onClick={removePerson}>Delete</button>
       </>
     );
   };
-
   const loading = () => {
     return <h1>Loading ...</h1>;
   };
 
   return (
     <div className="person">
-      {loaded()}
+      { person ? loaded() : loading() }
 
-      {isEditing && (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={editForm.name}
-            name="name"
-            placeholder="name"
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            value={editForm.image}
-            name="image"
-            placeholder="image URL"
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            value={editForm.title}
-            name="title"
-            placeholder="title"
-            onChange={handleChange}
-          />
-          <input type="submit" value="Update Person" />
-        </form>
-      )}
+      { isEditing && 
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={editForm.name}
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.image}
+          name="image"
+          placeholder="image URL"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.title}
+          name="title"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Update Person" />
+      </form>
+    }
+
     </div>
   );
 };
